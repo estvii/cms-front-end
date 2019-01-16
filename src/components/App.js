216b048deab7284a592dbe754f9c1efd3d3127f4
Allan Gao <allangao95@gmail.com>
2019-01-16 21:22:15 +1100

    material navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./navbar/Navbar";
 
-import ClientCreate from './client/ClientCreate';
+import ClientCreate from "./client/ClientCreate";
 
 const App = () => {
-    return(
-        <div className="ui container">
-            <BrowserRouter>
-                <Route path='/' exact component={ClientCreate} />
-            </BrowserRouter>
-        </div>
-    )
-}
+  return (
+    <div className="ui container">
+      <Navbar />
+      <BrowserRouter>
+        <Route path="/" exact component={ClientCreate} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
