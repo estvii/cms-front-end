2f741b2441f1d417dffe6440c776476733b83293
Steven Vi <visteven00@gmail.com>
2019-01-17 09:22:30 +1100

    client list

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ClientCreate from './client/ClientCreate';
 import ClientFilter from './client/ClientFilter';
+import ClientList from './client/ClientList';
 
 const App = () => {
     return(
         <div className="ui container">
             <BrowserRouter>
                 <Switch>
+                    <Route path='/' exact component={ClientList}/>
                     <Route path='/create' exact component={ClientCreate} />
                     <Route path='/client/filter/:id' exact component={ClientFilter} />
                 </Switch>
---
+++ src/components/client/ClientList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ClientList extends Component {
+    render() {
+        return(
+            <div>ClientList</div>
+        );
+    }
+}
+
+export default ClientList;
