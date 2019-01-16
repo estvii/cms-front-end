8bdb491923f3b1fc7b783fb02bc5f0658589213d
Steven Vi <visteven00@gmail.com>
2019-01-16 22:13:26 +1100

    Renaming Files

---
+++ src/components/client/ClientFilter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// Actions import
+// IMport form
+
+class ClientFilter extends Component {
+    render(){
+        return (
+            <div>ClientFilter</div>
+        );
+    }
+} 
+
+export default ClientFilter;
+
+// treat like the update
