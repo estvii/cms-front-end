c1b98a380c046d54cf8014bc3b1f7ca17f819257
Steven Vi <visteven00@gmail.com>
2019-01-17 09:29:00 +1100

    Fixed Conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
 
 import ClientCreate from './client/ClientCreate';
 import ClientFilter from './client/ClientFilter';
@@ -8,6 +8,7 @@ import ClientFilter from './client/ClientFilter';
 const App = () => {
     return(
         <div className="ui container">
+            <Navbar />
             <BrowserRouter>
                 <Switch>
                     <Route path='/create' exact component={ClientCreate} />
@@ -17,23 +18,5 @@ const App = () => {
         </div>
     )
 }
-=======
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Navbar from "./navbar/Navbar";
-
-import ClientCreate from "./client/ClientCreate";
-
-const App = () => {
-  return (
-    <div className="ui container">
-      <Navbar />
-      <BrowserRouter>
-        <Route path="/" exact component={ClientCreate} />
-      </BrowserRouter>
-    </div>
-  );
-};
->>>>>>> origin/nav-search-bar
 
 export default App;
