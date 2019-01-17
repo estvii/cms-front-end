c2e0c5751e643997019284af8d0277ac2e6ca588
Allan Gao <allangao95@gmail.com>
2019-01-17 16:52:18 +1100

    fixed merge

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
-
-import ClientCreate from './client/ClientCreate';
-import ClientFilter from './client/ClientFilter';
-import ClientList from './client/ClientList';
-
-const App = () => {
-    return(
-        <div className="ui container">
-            <Navbar />
-            <BrowserRouter>
-                <Switch>
-                    <Route path='/' exact component={ClientList}/>
-                    <Route path='/create' exact component={ClientCreate} />
-                    <Route path='/client/filter/:id' exact component={ClientFilter} />
-                </Switch>
-            </BrowserRouter>
-        </div>
-    )
-}
-=======
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./layout/Navbar/Navbar";
@@ -43,6 +19,5 @@ const App = () => {
     </div>
   );
 };
->>>>>>> origin/sidebar
 
 export default App;
