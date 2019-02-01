f8bfec45f1a6161a95b0498d3b207d0d9d2c1e60
Allan Gao <allangao95@gmail.com>
2019-02-01 11:10:30 +1100

    server messages form validated, now need to persist?

--- src/components/reports/ServerMessages.js
+++ src/components/reports/ServerMessages.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const snackBarStyle = {
   backgroundColor: "#c5cae9",
   margin: "5vh"
 };
+
+// renderTextField = formProps => {
+//   const { input, label } = formProps;
+//   return (
+//     <TextField
+//       id="outlined-multiline-flexible"
+//       label={label}
+//       multiline
+//       margin="normal"
+//       variant="outlined"
+//       {...input}
+//     />
+//   );
+// };
 
 class MessagesForm extends Component {
   state = {
@@ -32,28 +48,91 @@ class MessagesForm extends Component {
       <SnackbarContent style={snackBarStyle} message={this.state.messages} />
     );
     this.setState({
-      messagesList: list
+      messagesList: list,
+      messages: ""
     });
   };
 
+  // renderSendButton = () => {
+  //   if (this.state.messages.length > 0) {
+  //     return (
+  //       <Button
+  //         type="submit"
+  //         value="Submit"
+  //         onClick={this.addMessages}
+  //         color="primary"
+  //         disabled
+  //       >
+  //         SEND
+  //       </Button>
+  //     );
+  //   } else {
+  //     return (
+  //       <Button
+  //         type="submit"
+  //         value="Submit"
+  //         onClick={this.addMessages}
+  //         color="primary"
+  //         disabled={pristine || submitting}
+  //       >
+  //         SEND
+  //       </Button>
+  //     );
+  //   }
+  // };
+
   render() {
+    const { pristine, submitting } = this.props;
     return (
       <div>
         <div>
           <h1>{this.state.messagesList}</h1>
         </div>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <TextField
             type="text"
             value={this.state.value}
             onChange={this.handleChange}
-            placeholder="Leave a message"
+            label="Leave a message"
             value={this.state.messages}
             onChange={this.handleMessagesChange}
+            multiline
+            margin="normal"
+            variant="outlined"
+            autocomplete="off"
           />
-
-          <input type="submit" value="Submit" onClick={this.addMessages} />
         </form>
+        <div>
+          {this.state.messages.length > 0 ? (
+            <Button
+              type="submit"
+              value="Submit"
+              onClick={this.addMessages}
+              color="primary"
+            >
+              SEND
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              value="Submit"
+              onClick={this.addMessages}
+              color="primary"
+              disabled
+            >
+              SEND
+            </Button>
+          )}
+          {/* <Button
+          type="submit"
+          value="Submit"
+          onClick={this.addMessages}
+          color="primary"
+          disabled={pristine || submitting}
+        >
+          SEND
+        </Button> */}
+        </div>
       </div>
     );
   }
